8f6f4680c52c3ce538410555c2d53f810a3274f3
Nathan Loudon <vesnathan@gmail.com>
2022-06-21 22:31:47 +1000

    developing API requests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
 $(function(){
 
+    // call this function with the value from the search text input on click listener
+    function getCocktail(cocktailName){
+
+        // api key
+
+
+        // 
+
+    }
+
+    function getOtherData(){
+        // https://spoonacular.com/food-api/
+    }
 
 });
