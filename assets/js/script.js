7633ef9fdd46fb3ed03d62f7002164e641384290
Nathan Loudon <vesnathan@gmail.com>
2022-06-21 23:03:50 +1000

    developing API requests

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,43 @@ $(function(){
     // call this function with the value from the search text input on click listener
     function getCocktail(cocktailName){
 
-        // api key
+        var apiKey = "1";
 
-
-        // 
+        fetch(`https://www.thecocktaildb.com/api/json/v1/${apiKey}/search.php?s=${cocktailName}`)
+        .then (response => response.json())
+        .then((data) => {
+            console.log(data);
+            var drinkInstructions = data.drinks[0].strInstructions;
+            var instructionsHTML = `<h2>${cocktailName}</h2><p>${drinkInstructions}</p>`;
+            var imgSrc = data.drinks[0].strDrinkThumb;
+            var buttonListHTML = "";
+            for (var i = 1; i <= 15; i++ ){
+                var ingredient = data.drinks[0]["strIngredient"+i];
+                if (ingredient !== null) {
+                    buttonListHTML += `<button class="ingredientButton" data-ingredient="${ingredient}">${ingredient}</button>`;
+                }
+            }
+            console.log(buttonListHTML);
+        })
+        .catch((error) => {
+            console.error("Error: ", error);
+        })
+        
 
     }
 
-    function getOtherData(){
+    // call this function from the click listener for each ingredient 
+    function getIngredientData(ingredient){
+
+        // for testing
+        ingredient = "Sugar";
+
         // https://spoonacular.com/food-api/
-    }
+        var apiKey = "8745aeb27da04cd69ab054ceafed0495";
 
+        var html=`<p></p>`;
+
+        // insert into modal
+    }
+    getCocktail("Margarita");
 });
