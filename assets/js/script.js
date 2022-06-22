dbb937734aa273a86e515d94fa18c32aa13b3879
Nathan Loudon <vesnathan@gmail.com>
2022-06-22 11:47:57 +1000

    finished getCocktail function. See comments on issue for usage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,28 +4,76 @@ $(function(){
     function getCocktail(cocktailName){
 
         var apiKey = "1";
-
-        fetch(`https://www.thecocktaildb.com/api/json/v1/${apiKey}/search.php?s=${cocktailName}`)
+        var returnValue = {};
+        return fetch(`https://www.thecocktaildb.com/api/json/v1/${apiKey}/search.php?s=${cocktailName}`)
         .then (response => response.json())
         .then((data) => {
-            console.log(data);
-            var drinkInstructions = data.drinks[0].strInstructions;
-            var instructionsHTML = `<h2>${cocktailName}</h2><p>${drinkInstructions}</p>`;
-            var imgSrc = data.drinks[0].strDrinkThumb;
-            var buttonListHTML = "";
-            for (var i = 1; i <= 15; i++ ){
-                var ingredient = data.drinks[0]["strIngredient"+i];
-                if (ingredient !== null) {
-                    buttonListHTML += `<button class="ingredientButton" data-ingredient="${ingredient}">${ingredient}</button>`;
+            
+            // if there is more than one drink, add buttons to modal for user to select which one
+            // ie, "Margarita" will return half a dozen different versions
+            var drinksReturned = data.drinks.length;
+            if (drinksReturned >= 1) {
+                if (drinksReturned === 1) { 
+
+                    // one drink returned
+                    var strDrinkThumb = data.drinks[0].strDrinkThumb;
+                    var strInstructions = data.drinks[0].strInstructions
+                    var strDrink = data.drinks[0].strDrink
+
+                    // create ingredient array
+                    var strIngredients = [];
+                    for (var i = 1; i <= 15; i++) {
+                        var ingredientName = data.drinks[0]["strIngredient"+i];
+                        if (ingredientName !== null) {
+                            strIngredients.push(ingredientName);
+                        }
+                    }
+
+                    // return JSON object
+                    returnValue = {
+                        "status": "success",
+                        "strDrink": strDrink,
+                        "strDrinkThumb": strDrinkThumb,
+                        "strInstructions": strInstructions,
+                        "strIngredients": strIngredients                   
+                    }
                 }
-            }
-            console.log(buttonListHTML);
+                else {  
+                // multiple drinks returned
+                    var drinkList = [];
+                    data.drinks.map((drink) => {
+                        var drinkName = drink.strDrink;
+                        drinkList.push(drinkName); 
+                    });
+                    
+                    // append buttonListHTML to modal
+                    //console.log("buttonListHTML",buttonListHTML);
+                    returnValue = {
+                        "status": "success",
+                        "drinksList": drinkList                  
+                    };
+                    return returnValue;
+                    // show modal
+
+                 }
+             } else {
+                // no drinks returned
+
+                returnValue = {
+                     "status": "error",
+                     "errorMessage": "We couldn't find that Cocktail!"                  
+                };
+             }
+             return returnValue;
         })
         .catch((error) => {
-            console.error("Error: ", error);
-        })
+             returnValue = {
+                 "status": "error",
+                 "errorMessage": error                 
+             };
+        });
+       
         
-
     }
 
     // call this function from the click listener for each ingredient 
@@ -37,9 +85,10 @@ $(function(){
         // https://spoonacular.com/food-api/
         var apiKey = "8745aeb27da04cd69ab054ceafed0495";
 
-        var html=`<p></p>`;
-
         // insert into modal
     }
-    getCocktail("Margarita");
+    
+    // Example Usage
+    getCocktail("Margarita").then(response => console.log(response));
+
 });
