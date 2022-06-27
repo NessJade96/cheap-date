f47f8786c131175373e9ba86d1dd5abe45b90331
Vanessa Bloom <ness.jade@outlook.com>
2022-06-27 15:21:07 +1000

    Removing favs from local storage.
    Saved code to see the drinks displayed vertically and with x to remove them. In the process of doing this.

--- assets/js/script-fav.js
+++ assets/js/script-fav.js
@@ -1,9 +1,33 @@
 $(function () {
-	var storedCocktails = JSON.parse(localStorage.getItem("storedCocktails"));
-	storedCocktails.map((cocktail) => {
-		console.log(cocktail);
-		var favouriteDrinkItem =
-			"<button class='cocktailFavDrinksButton'>" + cocktail + "</button>";
-		$("#cocktailFavDiv").append(favouriteDrinkItem);
+	// var removeFavCocktail = $(".removeFavCocktail");
+	var favouriteDrinkItem;
+	var storedCocktails;
+	function loadDrinks() {
+		storedCocktails = JSON.parse(localStorage.getItem("storedCocktails"));
+		storedCocktails.map((cocktail) => {
+			console.log(cocktail);
+			favouriteDrinkItem =
+				"<div><button class='cocktailFavDrinksButton'>" +
+				cocktail +
+				"<button class='removeFavCocktail'> x</button></button></div>";
+			$("#cocktailFavDiv").append(favouriteDrinkItem);
+		});
+	}
+	loadDrinks();
+
+	$(".removeFavCocktail").on("click", function (event) {
+		eventPrev = event.prev;
+		storedCocktails = storedCocktails.splice(eventPrev);
+
+		// var currentTarget = $(event.currentTarget);
+		// var favCocktail = currentTarget.prev();
+		// console.log(typeof favCocktail);
+		// console.log(favCocktail);
+
+		// console.log(storedCocktails);
+		// storedCocktails = $.grep(storedCocktails, function (value) {
+		// 	return value != favouriteDrinkItem;
+		// });
+		// console.log(storedCocktails);
 	});
 });
