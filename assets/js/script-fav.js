27d9339fb7b03d4cb9fb5903bc29b170044610e2
Vanessa Bloom <ness.jade@outlook.com>
2022-06-28 13:48:11 +1000

    Drinks in Favourites page
    Added in a cross that will *soon* be able to remove drink from list and storage. Also removed the empty "" in array displaying.

--- assets/js/script-fav.js
+++ assets/js/script-fav.js
@@ -6,28 +6,31 @@ $(function () {
 		storedCocktails = JSON.parse(localStorage.getItem("storedCocktails"));
 		storedCocktails.map((cocktail) => {
 			console.log(cocktail);
-			favouriteDrinkItem =
-				"<div><button class='cocktailFavDrinksButton'>" +
-				cocktail +
-				"<button class='removeFavCocktail'> x</button></button></div>";
-			$("#cocktailFavDiv").append(favouriteDrinkItem);
+			if (cocktail != "") {
+				favouriteDrinkItem =
+					"<div><button class='cocktailFavDrinksButton'>" +
+					cocktail +
+					"<button class='removeFavCocktail'> x</button></button></div>";
+				$("#cocktailFavDiv").append(favouriteDrinkItem);
+			}
 		});
 	}
 	loadDrinks();
 
-	$(".removeFavCocktail").on("click", function (event) {
-		eventPrev = event.prev;
-		storedCocktails = storedCocktails.splice(eventPrev);
+	//This function below I am working on - this will remove the hearted drinks from list and local storage :)
 
-		// var currentTarget = $(event.currentTarget);
-		// var favCocktail = currentTarget.prev();
-		// console.log(typeof favCocktail);
-		// console.log(favCocktail);
+	// $(".removeFavCocktail").on("click", function (event) {
+	// 	eventPrev = event.prev;
+	// 	storedCocktails = storedCocktails.splice(eventPrev);
+	// 	// var currentTarget = $(event.currentTarget);
+	// 	// var favCocktail = currentTarget.prev();
+	// 	// console.log(typeof favCocktail);
+	// 	// console.log(favCocktail);
 
-		// console.log(storedCocktails);
-		// storedCocktails = $.grep(storedCocktails, function (value) {
-		// 	return value != favouriteDrinkItem;
-		// });
-		// console.log(storedCocktails);
-	});
+	// 	// console.log(storedCocktails);
+	// 	// storedCocktails = $.grep(storedCocktails, function (value) {
+	// 	// 	return value != favouriteDrinkItem;
+	// 	// });
+	// 	// console.log(storedCocktails);
+	// });
 });
